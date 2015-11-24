bd4f6f3b76a430d7b12278707e7ed39b31466d1d
Zhenyu Wen <zhenyuwen@dhcp-91-201.inf.ed.ac.uk>
2015-11-24 19:49:26 +0000

    newline

--- readFile.py
+++ readFile.py
@@ -4,10 +4,17 @@ __author__ = 'zhenyuwen'
 #file = open('/Users/zhenyuwen/Downloads/Harvard.txt', 'r')
 #print file.readline()
 
+tweet_list=[];
+tweet=[];
 with open("/Users/zhenyuwen/Downloads/Harvard.txt") as file:
-    # for line in file:
-    #    print line
-    data= file.readlines()
-    print data
+      for line in file:
+          print line
+          newline= line.replace('\t',"");
+          newline1=newline.replace('\r\n',"")
+       #    line.remove('\r\n');
+          l=newline1.split(" ")
+          print l
+    # data= file.readlines()
+    # print data
     #tweet= data.split("More");
     #print tweet
